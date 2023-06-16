2c6f0454333176a1184bc2d8ee05c24d6ff7707e
Ryley Banick <34217216+NoPanicBanick@users.noreply.github.com>
2023-06-15 23:34:44 -0500

    Revert "Remove azure"

--- providers.tf
+++ providers.tf
@@ -1,5 +1,9 @@
 terraform {
   required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.34.0"
+    }
     digitalocean = {
       source  = "digitalocean/digitalocean"
       version = "~> 2.0"
@@ -7,8 +11,20 @@ terraform {
   }
 }
 
+provider "azurerm" {
+  features {}
+}
+
 variable "do_token" {}
 
 provider "digitalocean" {
   token = var.do_token
+}
+
+provider "kubernetes" {
+  host  = digitalocean_kubernetes_cluster.foo.endpoint
+  token = digitalocean_kubernetes_cluster.foo.kube_config[0].token
+  cluster_ca_certificate = base64decode(
+    digitalocean_kubernetes_cluster.foo.kube_config[0].cluster_ca_certificate
+  )
 }
