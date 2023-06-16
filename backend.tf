59a2e18c98e52491cb9f0b7754e70fc6f50f3167
Ryley Banick <ryley.banick@icloud.com>
2023-06-15 21:43:05 -0700

    Updated the k8s version

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "terraform-state-rg"
     storage_account_name = "ryleyterraformstate"
     container_name       = "terraform"
-    key                  = "iac_banicry_aks.tfstate"
+    key                  = "iac_cluster.tfstate"
   }
 }
