58dfaf7ba1880398216aeac3b31d98cfba43427b
Ryley Banick <ryley.banick@icloud.com>
2023-06-15 22:21:35 -0700

    Adding a storage account for the overseerr

--- main.tf
+++ main.tf
@@ -14,3 +14,23 @@ resource "digitalocean_kubernetes_cluster" "k8s" {
     node_count = 1
   }
 }
+
+// Storage for overseerr config
+resource "azurerm_resource_group" "overseerr" {
+  name = "bc-overseerr-rg"
+  location = "westus"
+}
+
+resource "azurerm_storage_account" "overseerr" {
+  name                     = "${local.base_name}overseerr"
+  resource_group_name      = azurerm_resource_group.overseerr.name
+  location                 = azurerm_resource_group.overseerr.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_share" "backups" {
+  name                 = "overseerr"
+  storage_account_name = azurerm_storage_account.overseerr.name
+  quota                = 5 # 5 gigabytes
+}
